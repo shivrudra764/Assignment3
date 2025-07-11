6634f5984215be071e699ff87beb4d40ed9dbc78
shivrudra764 <shivrudra764@gmail.com>
2025-07-11 22:23:18 +0530

    Delete read_error.py

--- read_error.py
+++
@@ -1,9 +0,0 @@
-try:    #Checks the code if the file "sample.txt" exists or not
-    with open("sample.txt", "r") as file:   #Opens the file
-        content_list = file.readlines() #Convert the lines of file into a list
-        i = 1
-        for line in content_list:   #For Loop to print all the lines of file which are saved as a list
-            print(f"Line {i}: {line.rstrip()}") #Function rstrip() is used to remove the '/n'(next line) at the right end of each element of list
-            i += 1  #Increments the value of 'i' by 1
-except FileNotFoundError:   #Prints the error message if 'FileNotFoundError' error is encountered
-    print("Error: The 'sample.txt' was not found.")
